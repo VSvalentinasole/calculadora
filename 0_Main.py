4ab5383fa9ee703de836ab38179c382701edd36c
valentinassole <valentina.sole@ing.austral.edu.ar>
2024-04-07 20:16:52 -0300

    x

--- 0_Main.py
+++ 0_Main.py
@@ -23,26 +23,26 @@ def main():
         
         resultado = regla_de_tres_combustible(a)
         precio = round(resultado * 1.8)
-        amortizacion = int(a * 0.075)
+        amortizacion = round(a * 0.075)
         if a>=50:
-            cantidad_horas = (a/50)
-            st.write(f"Combustible: {int(resultado)} LITROS")
-            st.write(f"Precio combistible: {precio} EUROS")
-            st.write(f"Amortización: {amortizacion} EUROS")
-            st.write(f"Conductor: {round(cantidad_horas)} HORAS")
-            precio_por_horas = int(cantidad_horas * 15)
-            st.write(f"Precio conductor: {precio_por_horas} EUROS")
+            cantidad_horas = (a/60)
+            st.write(f"Combustible (8 LITROS/100km): {round(resultado)} LITROS")
+            st.write(f"Precio combistible (1.8 EUROS/LITRO): {precio} EUROS")
+            st.write(f"Amortización (7.5 CENT/KM): {amortizacion} EUROS")
+            st.write(f"Conductor (60KM/H): {round(cantidad_horas)} HORAS")
+            precio_por_horas = round(cantidad_horas * 15)
+            st.write(f"Precio conductor (15 EUROS/HORA): {precio_por_horas} EUROS")
             total = precio_por_horas + amortizacion + precio
             st.write(f"Total: {total} EUROS")
         
         else:
-            cantidad_horas = ((a*60)/50)
-            st.write(f"Combustible: {int(resultado)} LITROS")
-            st.write(f"Precio combistible: {precio} EUROS")
-            st.write(f"Amortización: {amortizacion} EUROS")
-            st.write(f"Conductor: {round(cantidad_horas)} MINUTOS")
-            precio_por_horas = int(cantidad_horas * 15)
-            st.write(f"Precio conductor: {precio_por_horas} EUROS")
+            cantidad_horas = ((a*60)/60)
+            st.write(f"Combustible (8 LITROS/100km): {round(resultado)} LITROS")
+            st.write(f"Precio combistible (1.8 EUROS/LITRO): {precio} EUROS")
+            st.write(f"Amortización (7.5 CENT/KM): {amortizacion} EUROS")
+            st.write(f"Conductor (60KM/H): {round(cantidad_horas)} MINUTOS")
+            precio_por_horas = round(cantidad_horas * 0.25)
+            st.write(f"Precio conductor  (15 EUROS/HORA): {precio_por_horas} EUROS")
             total = precio_por_horas + amortizacion + precio
             st.write(f"Total: {total} EUROS")
         
